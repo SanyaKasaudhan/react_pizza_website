2492ef2ac1169da72e4c3c3dd261b53f5d365278
Sanya <kasaudhansanya@gmail.com>
2020-11-09 12:16:03 +0530

    app data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Hero from './components/Hero'
 import {BrowserRouter as Router} from 'react-router-dom'
 import { GlobalStyle } from './globalStyles';
+import Products from './components/Products'
+import { productData } from './components/Products/data';
+
 function App() {
   return (
     <div className="App">
       <Router>
       <GlobalStyle />
       <Hero/>
+      <Products heading='Choose your favorite' data={productData}/>
       </Router>
     </div>
   );
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
-
-const Products = () => {
+import {ProductButton,
+    ProductCard,
+    ProductDesc,
+    ProductHeading,
+    ProductWrapper,
+    ProductImg,
+    ProductInfo,
+    ProductPrice,
+    ProductTitle,
+ProductsContainer}from './ProductElements'
+const Products = (heading,data) => {
     return (
         <div>
             <ProductsContainer>
-                <ProductHeading>heading</ProductHeading>
+                <ProductHeading>{heading}</ProductHeading>
                 <ProductWrapper>
-                    
+                    {data.map((product,index)=>{
+                        return (
+                            <ProductCard key={index}>
+                                <ProductImg src={product.img} alt={product.alt} />
+                                <ProductInfo>
+                                    <ProductTitle>{product.name}</ProductTitle>
+                                    <ProductDesc>{product.desc}</ProductDesc>
+                                    <ProductPrice>{product.price}</ProductPrice>
+                                    <ProductButton>{product.button}</ProductButton>
+                                </ProductInfo>
+                            </ProductCard>
+                        )
+                    })}
                 </ProductWrapper>
             </ProductsContainer>
         </div>
